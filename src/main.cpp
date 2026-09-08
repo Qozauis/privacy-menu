88afac3abf7f1609494e361b10dc0381e5d15a48
locallist <local@localhost>
2026-09-08 20:56:18 +0200

    Implement Geode firewall

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
 #include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
+
+$execute {
+    web::WebRequestInterceptEvent().listen(
+        [](auto, auto&) {
+            if (
+                Mod::get()->getSettingValue<bool>("privacy-lockdown")
+            ) {
+                return ListenerResult::Stop;
+            }
+
+            return ListenerResult::Propagate;
+        },
+        Priority::VeryEarly
+    );
+}
