3788668a8210c63ddf55f7c6c1197e95e897f8ad
locallist <local@localhost>
2026-09-08 21:08:55 +0200

    Block Cocos HTTP in privacy lockdown

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,43 @@
 #include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
+using namespace cocos2d::extension;
+
+void privacyCCHttpSend(
+    CCHttpClient* self,
+    CCHttpRequest* request
+) {
+    if (Mod::get()->getSettingValue<bool>("privacy-lockdown")) {
+        return;
+    }
+
+    self->send(request);
+}
 
 $execute {
+    // Geode WebRequest firewall
     web::WebRequestInterceptEvent().listen(
         [](auto, auto&) {
-            if (
-                Mod::get()->getSettingValue<bool>("privacy-lockdown")
-            ) {
+            if (Mod::get()->getSettingValue<bool>("privacy-lockdown")) {
                 return ListenerResult::Stop;
             }
 
             return ListenerResult::Propagate;
         },
         Priority::VeryEarly
+    );
+
+    // Geometry Dash / Cocos2D-X HTTP firewall
+    Mod::get()->hook(
+        reinterpret_cast<void*>(
+            geode::addresser::getNonVirtual(
+                geode::modifier::Resolve<CCHttpRequest*>::func(
+                    &CCHttpClient::send
+                )
+            )
+        ),
+        &privacyCCHttpSend,
+        "cocos2d::extension::CCHttpClient::send",
+        tulip::hook::TulipConvention::Thiscall
     );
 }
